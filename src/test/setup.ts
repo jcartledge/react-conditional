a0fcdf4b514a3de7701bfd667cc7a86a94c0e769
James Cartledge <jcartledge@gmail.com>
2023-06-28 15:20:40 +1000

    chore: remove obsolete type

--- src/test/setup.ts
+++ src/test/setup.ts
@@ -1,16 +1,6 @@
-import matchers, {
-  TestingLibraryMatchers,
-} from "@testing-library/jest-dom/matchers";
+import matchers from "@testing-library/jest-dom/matchers";
 import { cleanup } from "@testing-library/react";
 import { afterEach, expect } from "vitest";
-
-declare global {
-  namespace Vi {
-    interface JestAssertion<T = any>
-      extends jest.Matchers<void, T>,
-        TestingLibraryMatchers<T, void> {}
-  }
-}
 
 expect.extend(matchers);
 
